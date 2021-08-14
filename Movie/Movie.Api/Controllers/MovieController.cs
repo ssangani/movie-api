4da318d7177144d739531ec9141a57c43cebb4f3
Sagar Sangani <sagarsangani29@gmail.com>
2021-08-14 13:56:51 -0400

    Add shell MovieController

---
+++ Movie/Movie.Api/Controllers/MovieController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movie.Engine.Models;
+
+namespace Movie.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet("find")]
+        [ProducesResponseType(typeof(MovieInfo[]), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<MovieInfo[]>> Find(
+            [FromQuery] string titleLike,
+            [FromQuery] int yearOfRelease,
+            [FromQuery] string[] genres)
+        {
+            try
+            {
+                return NotFound();
+            }
+            catch (ArgumentException ae)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ae.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("top")]
+        [ProducesResponseType(typeof(MovieInfo[]), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<MovieInfo[]>> FindTopRated(
+            [FromQuery] int userId)
+        {
+            try
+            {
+                return NotFound();
+            }
+            catch (ArgumentException ae)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ae.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut]
+        [Route("user/{userId}/title/{titleId}/{rating}")]
+        public async Task<IActionResult> UpsertUserRating(
+            int userId,
+            int titleId,
+            int rating)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+            catch (ArgumentException ae)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ae.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ Movie/Movie.Engine/Models/MovieInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Movie.Engine.Models
+{
+    public class MovieInfo
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; init; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; init; }
+
+        [JsonPropertyName("yearOfRelease")]
+        public int ReleaseYear { get; init; }
+
+        [JsonPropertyName("runningTime")]
+        public TimeSpan RunningTime { get; init; }
+
+        [JsonPropertyName("genres")]
+        public Genre[] Genres { get; init; }
+
+        [JsonPropertyName("averageRating")]
+        public float AverageRating { get; init; }
+    }
+}

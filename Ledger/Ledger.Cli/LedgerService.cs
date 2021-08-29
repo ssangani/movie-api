122273a547556cb454d05e2d341c526fd5a9e4ea
Sagar Sangani <sagarsangani29@gmail.com>
2021-08-29 18:42:10 -0400

    Converting to executable
    * Adding DI extensions such as config and logging
    * Moving csv to app content

---
+++ Ledger/Ledger.Cli/LedgerService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Ledger.Cli
+{
+  public class LedgerService : IHostedService
+  {
+    private readonly ILogger _logger;
+    private readonly IHostApplicationLifetime _appLifetime;
+
+    private int? _exitCode;
+
+    public LedgerService(
+      ILogger<LedgerService> logger,
+      IHostApplicationLifetime appLifetime)
+    {
+      _logger = logger;
+      _appLifetime = appLifetime;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+      _logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+
+      _ = _appLifetime.ApplicationStarted.Register(async () => await RunAsync(cancellationToken));
+
+      return Task.CompletedTask;
+    }
+
+    public async Task RunAsync(CancellationToken cancellationToken)
+    {
+      try
+      {
+        await Task.Yield();
+
+        _exitCode = 0;
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError(ex, "Unhandled exception!");
+        _exitCode = 1;
+      }
+      finally
+      {
+        // Stop the application once the work is done
+        _appLifetime.StopApplication();
+      }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+      _logger.LogDebug($"Exiting with return code: {_exitCode}");
+
+      Environment.ExitCode = _exitCode.GetValueOrDefault(-1);
+      return Task.CompletedTask;
+    }
+  }
+}
--- Ledger/Ledger.Cli/Program.cs
+++ Ledger/Ledger.Cli/Program.cs
@@ -1,27 +1,55 @@
 ﻿using System;
-using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Ledger.Engine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Ledger.Cli
 {
   public class Program
   {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
       Console.WriteLine("Hello");
 
-      CreateHostBuilder(args)
+      await CreateHosBuilder(args)
         .Build()
-        .Run();
+        .RunAsync();
 
       Console.WriteLine("Bye");
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+    private static IHostBuilder CreateHosBuilder(string[] args) {
+      var builtConfig = new ConfigurationBuilder()
+        .AddEnvironmentVariables()
+        .AddJsonFile("appsettings.json")
+        .AddCommandLine(args)
+        .Build();
+
+      Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builtConfig)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+
+      return Host
+        .CreateDefaultBuilder(args)
+        .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+        .UseSerilog()
+        .ConfigureAppConfiguration(builder =>
+        {
+          builder.AddConfiguration(builtConfig);
+        })
+        .ConfigureServices((hostContext, services) =>
+        {
+          services
+            .AddLedgerServices(builtConfig)
+            .AddHostedService<LedgerService>();
+        });
+    }
   }
 }

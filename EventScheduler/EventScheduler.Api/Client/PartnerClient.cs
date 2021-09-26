08258d510c92e6e856fdb3c19df384bbb93d7b94
Sagar Sangani <sagarsangani29@gmail.com>
2021-09-26 15:36:27 -0400

    Add clients and controller

---
+++ EventScheduler/EventScheduler.Api/Client/PartnerClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using EventScheduler.Api.Models;
+
+namespace EventScheduler.Api.Client
+{
+  public interface IPartnerClient
+  {
+    public Task<PartnerAvailabilities> GetPartnerAvailabilities(CancellationToken ctx);
+  }
+
+  public class PartnerClient : IPartnerClient
+  {
+    // TODO: Move secret to appsettings
+    private const string _uri = "dataset?userKey=c82cdf8b6a3e283c4d81169a6406";
+    private readonly HttpClient _client;
+
+    public PartnerClient(
+      HttpClient client)
+    {
+      _client = client;
+    }
+
+    public async Task<PartnerAvailabilities> GetPartnerAvailabilities(CancellationToken ctx)
+    {
+      var response = await _client.GetAsync(_uri, ctx);
+      response.EnsureSuccessStatusCode();
+
+      return JsonSerializer.Deserialize<PartnerAvailabilities>(
+        await response.Content.ReadAsStringAsync(ctx));
+    }
+  }
+}
---
+++ EventScheduler/EventScheduler.Api/Controllers/EventSchedulerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EventScheduler.Api.Calculators;
+using EventScheduler.Api.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventScheduler.Api.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EventSchedulerController : ControllerBase
+  {
+    private readonly IEventSchedulerClient _eventSchedulerClient;
+    private readonly IPartnerClient _partnerClient;
+    private readonly IEventPlanner _eventPlanner;
+
+    public EventSchedulerController(
+      IEventSchedulerClient eventSchedulerClient,
+      IPartnerClient partnerClient,
+      IEventPlanner eventPlanner)
+    {
+      _eventSchedulerClient = eventSchedulerClient;
+      _partnerClient = partnerClient;
+      _eventPlanner = eventPlanner;
+    }
+
+    [HttpPost]
+    public async Task ScheduleAsync(CancellationToken ctx)
+    {
+      var availabilities = await _partnerClient.GetPartnerAvailabilities(ctx);
+      var schedule = _eventPlanner.Schedule(availabilities);
+      await _eventSchedulerClient.ScheduleAsync(schedule, ctx);
+    }
+  }
+}
